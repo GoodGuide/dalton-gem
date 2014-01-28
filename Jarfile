98bde2af0622a2f42e2de6104122754a82710023
Brian Jenkins <bonkydog@bonkydog.com>
2014-01-28 09:32:02 -0800

    Update datomic.

--- Jarfile
+++ Jarfile
@@ -2,4 +2,4 @@ repository 'https://repository.jboss.org/nexus/content/groups/public-jboss'
 remote_repo 'http://files.couchbase.com/maven2/'
 
 # Default group is default
-jar "com.datomic:datomic-pro:0.9.4384"
+jar "com.datomic:datomic-pro:0.9.4770"
--- clojure/project.clj
+++ clojure/project.clj
@@ -1,7 +1,8 @@
 (defproject datomizer "0.1.0-SNAPSHOT"
   :description "Simple Datomic adapter and marshalling for JRuby"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.9.4384"]]
+                 [com.datomic/datomic-pro "0.9.4470"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [org.clojure/java.classpath "0.2.1"]]}})
+                                  [org.clojure/java.classpath "0.2.1"]]}}
+  :jvm-opts ["-Xmx1g"])
