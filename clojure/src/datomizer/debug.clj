1223c031d22432ddffebf37d36ab04d363f1cea8
Brian Jenkins <bonkydog@bonkydog.com>
2014-01-29 13:26:25 -0800

    Turn debug on and off by binding *debug*.

--- clojure/src/datomizer/debug.clj
+++ clojure/src/datomizer/debug.clj
@@ -2,22 +2,26 @@
   "Debugging tools."
   (:require [clojure.pprint :refer [pprint]]))
 
+(def ^:dynamic *debug* false)
+
 (defn dbp [x]
   (println x)
   (flush))
 
 (defmacro dbg [& body]
   `(let [x# ~@body]
-     (print (str "dbg: " (quote ~@body) " = "))
-     (pprint x#)
-     (print "\n\n")
-     (flush)
+     (when *debug*
+       (print (str "dbg: " (quote ~@body) " = "))
+       (pprint x#)
+       (print "\n\n")
+       (flush))
      x#))
 
 (defmacro dbgv [& body]
   `(let [x# ~@body]
-     (print (str "dbgv: " (quote ~@body) " = "))
-     (pprint (if (seq? x#) (vec x#) x# ))
-     (print "\n\n")
-     (flush)
+     (when *debug*
+       (print (str "dbgv: " (quote ~@body) " = "))
+       (pprint (if (seq? x#) (vec x#) x# ))
+       (print "\n\n")
+       (flush))
      x#))
