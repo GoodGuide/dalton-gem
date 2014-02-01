3353bfcad90b63ffa783cd1c8ba6b2c5e1f1c9d0
Brian Jenkins <bonkydog@bonkydog.com>
2014-01-31 16:38:40 -0800

    Install datomize as a db fn (requires jar!)

--- clojure/src/datomizer/datomize.clj
+++ clojure/src/datomizer/datomize.clj
@@ -209,3 +209,27 @@
 (defn undatomize
   [entity]
   (apply hash-map (mapcat (fn [k] [k (decode-elements entity k (get entity (str k)))]) (conj (keys entity) :db/id))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Database functions
+
+(def datomize-db-fn
+  (d/function {:lang "clojure"
+               :params '[db entity]
+               :requires '[[datomizer.datomize]]
+               :code "(datomizer.datomize/datomize db entity)"}))
+
+(defn install-database-functions [dbc]
+  (d/transact dbc [{:db/id (d/tempid :db.part/user)
+                   :db/ident :dmzr.datomize
+                    :db/fn datomize-db-fn}]))
+
+(defn datomize-with-db-fn [dbc]
+  (let [f (:db/fn (d/entity (db dbc) :dmzr.datomize))]
+    f
+    (.invoke f (db dbc) {:db/id (d/tempid :db.part/user) :test/map {:a 1}})))
+
+(comment
+  (d/transact dbc [[:dmzr.datomize {:db/id (d/tempid :db.part/user) :test/map {:a 1}}]])
+  (undatomize (d/entity (db dbc) 17592186046111)))
+
