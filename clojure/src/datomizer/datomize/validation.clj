1f7ad512ea5b608b87e4d2b923817895040141e6
Brian Jenkins <bonkydog@bonkydog.com>
2014-02-06 17:04:40 -0800

    Extract validation to a module

---
+++ clojure/src/datomizer/datomize/validation.clj
@@ -0,0 +1,51 @@
+(ns datomizer.datomize.validation
+  "Short package description."
+  (:require [datomic.api :as d :refer [q]]))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Integrity checks
+
+(defn retrieve-all-elements [db]
+  (let [rules '[[[element? ?e] [_ :test/map ?e]]]]
+    (map (fn [x] (->> x first (d/entity db) d/touch)) (q '[:find ?e :in $ % :where (element? ?e)] db rules))))
+
+
+(defn valid-map? [db element]
+  (let [attributes (apply hash-set (keys element))]
+    (or (:ref/empty element)
+        (and (contains? attributes :element.map/key)
+             (not (contains? attributes :element.vector/index))
+             (some #(re-matches #"^:element\.value/.*" (str %)) attributes)))))
+
+(defn valid-vector? [db element]
+  (let [attributes (apply hash-set (keys element))]
+    (or (:ref/empty element)
+        (and (not (contains? (keys element) :element.map/key))
+             (contains? attributes :element.vector/index)
+             (some #(re-matches #"^:element\.value/.*" (str %)) attributes)))
+    :ref.type/value (and (not (contains? attributes :element.map/key))
+                         (not (contains? attributes :element.vector/index))
+                         (some #(re-matches #"^:element\.value/.*" (str %)) attributes))))
+
+
+(defn valid-value? [db element]
+  (let [attributes (apply hash-set (keys element))]
+    (and (not (contains? attributes :element.map/key))
+         (not (contains? attributes :element.vector/index))
+         (some #(re-matches #"^:element\.value/.*" (str %)) attributes))))
+
+(defn valid? [db element]
+  (let [references (d/datoms db :vaet (:db/id element))
+        ownerships (filter (fn [datom] (:is-component  (d/attribute db (.a datom)))) references)
+        ownership (first ownerships)
+        ownership-type (:ref/type (d/entity db (.a ownership)))
+        attributes (apply hash-set (keys element))]
+    (and  (= 1 (count ownerships))
+          (case ownership-type
+            :ref.type/map (valid-map? db element)
+            :ref.type/vector (valid-vector? db element)
+            :ref.type/value (valid-value? db element)))))
+
+(defn invalid-elements [db]
+  (remove (partial valid? db) (retrieve-all-elements db)))
