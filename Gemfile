d4ec419060753b99f845c34fcb1b0b58c629db51
http://jneen.net/ <jneen@jneen.net>
2014-06-11 12:17:29 -0700

    add wrong and pry, and implement `rake spec`

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gemspec
 gem 'rspec', '>=2.14.1'
+gem 'wrong'
+gem 'pry'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'bundler'
 require 'jbundler'
 Bundler.require
 
+require 'wrong/adapters/rspec'
+
 require 'dalton'
 
 Dir[File.join(File.dirname(__FILE__), 'support/**/*')].each {|f| require(f)}
