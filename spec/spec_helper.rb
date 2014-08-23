4bcd278b5bef411524bf66a99eeea6259df87828
http://jneen.net/ <jneen@jneen.net>
2014-08-23 10:07:36 -0700

    protect Dalton from being reopened

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ Bundler.require
 require 'wrong/adapters/rspec'
 
 def reload!
+  Object.send(:remove_const, :Dalton) if defined?(Dalton)
   load './lib/dalton.rb'
 
   Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each {|f| load(f)}
