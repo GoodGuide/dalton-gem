b7c43d8813cdbed8bcac79cede235d2f2e92d2d1
http://jneen.net/ <jneen@jneen.net>
2014-08-26 11:45:52 -0700

    in tests, log to log/test.log and leave backtraces alone

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,18 @@ def reload!
   Object.send(:remove_const, :Dalton) if defined?(Dalton)
   load './lib/dalton.rb'
 
+  Dalton::Model.logger = Logger.new('log/test.log')
+  Dalton::Model.logger.level = Logger::DEBUG
+  Dalton::Model.logger.info "**** Reloading Dalton ****"
+
   Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each {|f| load(f)}
 
   true
 end
 
 reload!
+
+RSpec.configure do |config|
+  config.backtrace_exclusion_patterns = []
+  config.backtrace_inclusion_patterns = []
+end
